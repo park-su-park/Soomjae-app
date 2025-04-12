0379f39873d34013df7f58c833dde796cf97f96a
ruh0n <wns8256@naver.com>
2025-04-12 11:50:45 +0900

    feat: add FlowUseCase and UseCase

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
     id("soomjae.multiplatform.core")
 }
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.core.common)
+        }
+    }
+}
---
+++ core/domain/src/commonMain/kotlin/com/parksupark/soomjae/core/domain/failures/Failure.kt
@@ -0,0 +1,5 @@
+package com.parksupark.soomjae.core.domain.failures
+
+interface Failure {
+    data object Unknown : Failure
+}
