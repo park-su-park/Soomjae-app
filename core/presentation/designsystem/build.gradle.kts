c9a4f61edf01f4c437b82b1bf06f06b8fbda3b74
ruh0n <wns8256@naver.com>
2025-04-12 23:34:19 +0900

    chore(deps): add MaterialKolor dependency

--- core/presentation/designsystem/build.gradle.kts
+++ core/presentation/designsystem/build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
     id("soomjae.multiplatform.ui")
 }
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.materialkolor)
+        }
+    }
+}
