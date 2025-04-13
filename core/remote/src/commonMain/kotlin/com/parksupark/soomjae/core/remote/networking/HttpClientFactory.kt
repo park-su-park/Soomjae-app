90348ad3d3ee276f5db1416b4b94567fdc40e091
ruh0n <wns8256@naver.com>
2025-04-13 09:53:28 +0900

    feat(remote): add HTTP client factory to use ktor

---
+++ core/remote/src/commonMain/kotlin/com/parksupark/soomjae/core/remote/networking/HttpClientFactory.kt
@@ -0,0 +1,41 @@
+package com.parksupark.soomjae.core.remote.networking
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngineConfig
+import io.ktor.client.engine.HttpClientEngineFactory
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import co.touchlab.kermit.Logger as Kermit
+
+expect fun platformHttpClientEngine(): HttpClientEngineFactory<HttpClientEngineConfig>
+
+internal class HttpClientFactory {
+    fun build(): HttpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(
+                json = Json {
+                    ignoreUnknownKeys = true
+                },
+            )
+        }
+        install(Logging) {
+            logger = object : Logger {
+                override fun log(message: String) {
+                    Kermit.i(message)
+                }
+            }
+            level = LogLevel.ALL
+        }
+
+        defaultRequest {
+            contentType(ContentType.Application.Json)
+        }
+    }
+}
