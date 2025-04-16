2efc7314fc650dcb33840827cc521ebfb4de2b57
ruh0n <wns8256@naver.com>
2025-04-16 20:57:25 +0900

    fix(core): fix miss imported dependency from ui to designsystem

--- core/presentation/ui/build.gradle.kts
+++ core/presentation/ui/build.gradle.kts
@@ -6,7 +6,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(projects.core.common)
-            implementation(projects.core.presentation.ui)
+            api(projects.core.presentation.designsystem)
         }
     }
 }
