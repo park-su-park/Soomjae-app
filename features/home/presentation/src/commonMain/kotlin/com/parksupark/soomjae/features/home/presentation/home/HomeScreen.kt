6b7d95318cd0dc7311c2c51fd33795cd254055e2
ruh0n <wns8256@naver.com>
2025-04-17 20:36:04 +0900

    feat(home): add searchbar to HomeScreen

--- features/home/presentation/src/commonMain/kotlin/com/parksupark/soomjae/features/home/presentation/home/HomeScreen.kt
+++ features/home/presentation/src/commonMain/kotlin/com/parksupark/soomjae/features/home/presentation/home/HomeScreen.kt
@@ -1,11 +1,39 @@
 package com.parksupark.soomjae.features.home.presentation.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.parksupark.soomjae.core.presentation.designsystem.components.SoomjaeOutlinedTextField
+import com.parksupark.soomjae.core.presentation.designsystem.components.SoomjaeScaffold
+import com.parksupark.soomjae.features.home.presentation.resources.Res
+import com.parksupark.soomjae.features.home.presentation.resources.home_search_hint
 
 @Composable
 internal fun HomeScreen(
     state: HomeState,
     onAction: (HomeAction) -> Unit,
 ) {
-    // TODO UI Rendering
+    SoomjaeScaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            SoomjaeOutlinedTextField(
+                state = rememberTextFieldState(),
+                startIcon = Icons.Default.Search,
+                hint = Res.string.home_search_hint.toString(),
+                modifier = Modifier.padding(horizontal = 16.dp),
+            )
+            Spacer(modifier = Modifier.weight(2f))
+        }
+    }
 }
