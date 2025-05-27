f7d851d1d4ecf935d9f6b2b7487445c0fede973a
ruh0n <wns8256@naver.com>
2025-05-28 00:31:16 +0900

    feat(profile): add profile navigation item to main navigation bar

--- composeApp/src/commonMain/kotlin/com/parksupark/soomjae/navigation/MainNavigationBarItem.kt
+++ composeApp/src/commonMain/kotlin/com/parksupark/soomjae/navigation/MainNavigationBarItem.kt
@@ -1,14 +1,18 @@
 package com.parksupark.soomjae.navigation
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.parksupark.soomjae.composeApp.resources.Res
 import com.parksupark.soomjae.composeApp.resources.navigation_bar_home
+import com.parksupark.soomjae.composeApp.resources.navigation_bar_profile
 import com.parksupark.soomjae.core.presentation.ui.navigation.NavigationBarItem
 import com.parksupark.soomjae.core.presentation.ui.navigation.NavigationDestination
 import com.parksupark.soomjae.features.home.presentation.navigation.HomeDestination
+import com.parksupark.soomjae.features.profile.presentation.navigation.ProfileDestination
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 import org.jetbrains.compose.resources.StringResource
@@ -28,5 +32,11 @@ internal val mainNavigationBarItems: ImmutableList<MainNavigationBarItem> = pers
         selectedIcon = Icons.Filled.Home,
         unselectedIcon = Icons.Outlined.Home,
         label = Res.string.navigation_bar_home,
+    ),
+    MainNavigationBarItem(
+        route = ProfileDestination.Root,
+        selectedIcon = Icons.Filled.AccountCircle,
+        unselectedIcon = Icons.Outlined.AccountCircle,
+        label = Res.string.navigation_bar_profile,
     ),
 )
