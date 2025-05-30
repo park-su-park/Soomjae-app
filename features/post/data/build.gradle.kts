ca85a11bdd1cb15eb8096ea9f34deb628a8626cb
ruh0n <wns8256@naver.com>
2025-05-30 10:36:34 +0900

    feat(post): add features post data module

---
+++ features/post/data/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("soomjae.multiplatform.data.remote")
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.core.domain)
+            implementation(projects.core.remote)
+
+            implementation(projects.features.post.domain)
+        }
+    }
+}
