9455c10f37c7befb054ee2221db563ae96a5fbe4
ruh0n <wns8256@naver.com>
2025-05-30 10:44:44 +0900

    feat(post): add di module for post features

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -20,6 +20,7 @@ kotlin {
             implementation(projects.features.auth.data)
             implementation(projects.features.auth.presentation)
             implementation(projects.features.home.presentation)
+            implementation(projects.features.post.data)
             implementation(projects.features.post.presentation)
             implementation(projects.features.profile.presentation)
         }
