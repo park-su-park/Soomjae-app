225d69dffd8acaa6e3d57bbe9895c0c813dabc0f
ruh0n <wns8256@naver.com>
2025-05-30 13:26:49 +0900

    feat(post): implement loading logic for community posts in CommunityTab

--- features/post/presentation/src/commonMain/kotlin/com/parksupark/soomjae/features/post/presentation/post/tabs/community/CommunityTabScreen.kt
+++ features/post/presentation/src/commonMain/kotlin/com/parksupark/soomjae/features/post/presentation/post/tabs/community/CommunityTabScreen.kt
@@ -1,13 +1,54 @@
 package com.parksupark.soomjae.features.post.presentation.post.tabs.community
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import app.cash.paging.LoadStateError
+import app.cash.paging.LoadStateLoading
+import app.cash.paging.LoadStateNotLoading
+import app.cash.paging.compose.LazyPagingItems
+import app.cash.paging.compose.itemKey
+import com.parksupark.soomjae.core.presentation.designsystem.components.SoomjaeScaffold
+import com.parksupark.soomjae.features.post.presentation.post.models.CommunityPostUi
 
 @Composable
 internal fun CommunityTabScreen(
     state: CommunityTabState,
     onAction: (CommunityTabAction) -> Unit,
+    posts: LazyPagingItems<CommunityPostUi>,
 ) {
-    Text("Community Tab Screen")
-    // TODO UI Rendering
+    SoomjaeScaffold { innerPadding ->
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = innerPadding,
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+        ) {
+            when (val loadState = posts.loadState.refresh) {
+                LoadStateLoading -> item { CircularProgressIndicator() }
+
+                is LoadStateNotLoading -> items(count = posts.itemCount, key = posts.itemKey { it.id }) { index ->
+                    val post = posts[index]!!
+                    Row(Modifier.fillMaxWidth()) {
+                        Text(
+                            post.id,
+                            Modifier.weight(1f),
+                        )
+                    }
+                }
+
+                is LoadStateError -> item {
+                    Text(loadState.error.message!!)
+                }
+
+                else -> error("when should be exhaustive")
+            }
+        }
+    }
 }
