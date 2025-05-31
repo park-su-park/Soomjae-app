2d09506a41f3d70bb2d899f31d716c123fcba3d5
ruh0n <wns8256@naver.com>
2025-06-01 00:06:44 +0900

    fix(community-detail): implement bearer authentication for HttpClient

--- core/remote/src/commonMain/kotlin/com/parksupark/soomjae/core/remote/networking/HttpClientFactory.kt
+++ core/remote/src/commonMain/kotlin/com/parksupark/soomjae/core/remote/networking/HttpClientFactory.kt
@@ -1,8 +1,12 @@
 package com.parksupark.soomjae.core.remote.networking
 
+import com.parksupark.soomjae.core.domain.auth.repositories.SessionRepository
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngineConfig
 import io.ktor.client.engine.HttpClientEngineFactory
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
@@ -16,7 +20,9 @@ import co.touchlab.kermit.Logger as Kermit
 
 expect fun platformHttpClientEngine(): HttpClientEngineFactory<HttpClientEngineConfig>
 
-internal class HttpClientFactory {
+internal class HttpClientFactory(
+    private val sessionRepository: SessionRepository,
+) {
     fun build(): HttpClient = HttpClient(platformHttpClientEngine()) {
         install(ContentNegotiation) {
             json(
@@ -32,6 +38,17 @@ internal class HttpClientFactory {
                 }
             }
             level = LogLevel.ALL
+        }
+        install(Auth) {
+            bearer {
+                loadTokens {
+                    val info = sessionRepository.get()
+                    BearerTokens(
+                        accessToken = info?.accessToken ?: "",
+                        refreshToken = null,
+                    )
+                }
+            }
         }
 
         defaultRequest {
