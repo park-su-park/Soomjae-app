665b90bde3fdf3fc1777cb136c066be31dc2397c
ruh0n <wns8256@naver.com>
2025-06-01 13:28:02 +0900

    fix(category): update category response dto

--- features/post/data/src/commonMain/kotlin/com/parksupark/soomjae/features/post/data/sources/CategoryRemoteSourceImpl.kt
+++ features/post/data/src/commonMain/kotlin/com/parksupark/soomjae/features/post/data/sources/CategoryRemoteSourceImpl.kt
@@ -3,7 +3,7 @@ package com.parksupark.soomjae.features.post.data.sources
 import arrow.core.Either
 import com.parksupark.soomjae.core.domain.failures.DataFailure
 import com.parksupark.soomjae.core.remote.networking.get
-import com.parksupark.soomjae.features.post.data.dtos.CategoriesResponse
+import com.parksupark.soomjae.features.post.data.dtos.CategoryResponse
 import com.parksupark.soomjae.features.post.data.dtos.toDomain
 import com.parksupark.soomjae.features.post.domain.models.Category
 import io.ktor.client.HttpClient
@@ -11,9 +11,11 @@ import io.ktor.client.HttpClient
 class CategoryRemoteSourceImpl(
     private val httpClient: HttpClient,
 ) : CategoryRemoteSource {
-    override suspend fun getAllCategories(): Either<DataFailure.Network, Map<Long, Category>> = httpClient.get<CategoriesResponse>(
+    override suspend fun getAllCategories(): Either<DataFailure.Network, Map<Long, Category>> = httpClient.get<CategoryResponse>(
         route = "/v1/categories/all",
     ).map { response ->
-        response.categories.map { it.toDomain() }.associateBy { it.id }
+        response.children.map { category ->
+            category.toDomain()
+        }.associateBy { it.id }
     }
 }
