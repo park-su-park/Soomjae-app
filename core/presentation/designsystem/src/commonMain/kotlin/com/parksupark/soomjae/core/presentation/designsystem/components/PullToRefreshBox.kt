16145dc9757b3d35dac122188ed0e4bd5ae7e835
ruh0n <wns8256@naver.com>
2025-06-01 17:03:36 +0900

    feat(designsystem): add custom PullToRefreshBox and indicator components

---
+++ core/presentation/designsystem/src/commonMain/kotlin/com/parksupark/soomjae/core/presentation/designsystem/components/PullToRefreshBox.kt
@@ -0,0 +1,71 @@
+package com.parksupark.soomjae.core.presentation.designsystem.components
+
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.PositionalThreshold
+import androidx.compose.material3.pulltorefresh.PullToRefreshState
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import com.parksupark.soomjae.core.presentation.designsystem.theme.SoomjaeTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SoomjaePullToRefreshBox(
+    isRefreshing: Boolean,
+    onRefresh: () -> Unit,
+    modifier: Modifier = Modifier,
+    state: PullToRefreshState = rememberPullToRefreshState(),
+    contentAlignment: Alignment = Alignment.TopStart,
+    indicator: @Composable BoxScope.() -> Unit = {
+        SoomjaeIndicator(
+            modifier = Modifier.align(Alignment.TopCenter),
+            isRefreshing = isRefreshing,
+            state = state,
+        )
+    },
+    content: @Composable BoxScope.() -> Unit,
+) {
+    PullToRefreshBox(
+        isRefreshing = isRefreshing,
+        onRefresh = onRefresh,
+        modifier = modifier,
+        state = state,
+        contentAlignment = contentAlignment,
+        indicator = indicator,
+        content = content,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SoomjaeIndicator(
+    state: PullToRefreshState,
+    isRefreshing: Boolean,
+    modifier: Modifier = Modifier,
+    containerColor: Color = SoomjaePullToRefreshDefaults.containerColor,
+    color: Color = SoomjaePullToRefreshDefaults.indicatorColor,
+    threshold: Dp = PositionalThreshold,
+) {
+    Indicator(
+        modifier = modifier,
+        isRefreshing = isRefreshing,
+        state = state,
+        containerColor = containerColor,
+        color = color,
+        threshold = threshold,
+    )
+}
+
+object SoomjaePullToRefreshDefaults {
+    val containerColor: Color
+        @Composable get() = SoomjaeTheme.colorScheme.background4
+
+    val indicatorColor: Color
+        @Composable get() = SoomjaeTheme.colorScheme.cta
+}
