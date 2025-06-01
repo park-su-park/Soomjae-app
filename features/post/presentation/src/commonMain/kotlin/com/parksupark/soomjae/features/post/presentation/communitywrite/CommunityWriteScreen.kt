f6783629712c10682bcae45e937fef7f69473bd2
ruh0n <wns8256@naver.com>
2025-06-01 10:31:21 +0900

    fix(community-write): fix colors for category button

--- features/post/presentation/src/commonMain/kotlin/com/parksupark/soomjae/features/post/presentation/communitywrite/CommunityWriteScreen.kt
+++ features/post/presentation/src/commonMain/kotlin/com/parksupark/soomjae/features/post/presentation/communitywrite/CommunityWriteScreen.kt
@@ -1,5 +1,6 @@
 package com.parksupark.soomjae.features.post.presentation.communitywrite
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,10 +11,13 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,11 +26,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.parksupark.soomjae.core.presentation.designsystem.components.SoomjaeCenterAlignedTopAppBar
 import com.parksupark.soomjae.core.presentation.designsystem.components.SoomjaeScaffold
 import com.parksupark.soomjae.core.presentation.designsystem.components.SoomjaeSurface
+import com.parksupark.soomjae.core.presentation.designsystem.theme.SoomjaeTheme
 import com.parksupark.soomjae.core.presentation.ui.resources.value
 import com.parksupark.soomjae.features.post.presentation.components.WriteInputSection
 import com.parksupark.soomjae.features.post.presentation.resources.Res
@@ -115,11 +121,23 @@ private fun AdditionalInfoSelection(
         OutlinedButton(
             onClick = { isCategoryVisible = categories.isNotEmpty() && true },
             modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.outlinedButtonColors().copy(
+                containerColor = Color.Transparent,
+                contentColor = SoomjaeTheme.colorScheme.text2,
+            ),
+            border = BorderStroke(1.dp, SoomjaeTheme.colorScheme.text2),
         ) {
+            val modifier = Modifier.fillMaxWidth()
             if (selectedCategory == null) {
-                Text(text = Res.string.community_write_category_select_placeholder.value)
+                Text(
+                    text = Res.string.community_write_category_select_placeholder.value,
+                    style = LocalTextStyle.current.copy(
+                        color = LocalContentColor.current.copy(alpha = 0.4f),
+                    ),
+                    modifier = modifier,
+                )
             } else {
-                Text(text = selectedCategory.name)
+                Text(text = selectedCategory.name, modifier = modifier)
             }
         }
 
