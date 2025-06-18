77fddba05c47b7348107b087cd04a03f4860c676
ruh0n <wns8256@naver.com>
2025-06-18 20:43:15 +0900

    feat(core-data): add setting datastore

---
+++ core/data/src/commonMain/kotlin/com/parksupark/soomjae/core/data/datastore/SettingDataStore.kt
@@ -0,0 +1,12 @@
+package com.parksupark.soomjae.core.data.datastore
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import okio.Path.Companion.toPath
+
+const val SETTING_DATA_STORE = "setting.preferences_pb"
+
+internal fun createSettingDataStore(producePath: () -> String): DataStore<Preferences> = PreferenceDataStoreFactory.createWithPath(
+    produceFile = { producePath().toPath() },
+)
--- core/data/src/desktopMain/kotlin/com/parksupark/soomjae/core/data/di/CoreDataModule.desktop.kt
+++ core/data/src/desktopMain/kotlin/com/parksupark/soomjae/core/data/di/CoreDataModule.desktop.kt
@@ -1,11 +1,14 @@
 package com.parksupark.soomjae.core.data.di
 
 import com.parksupark.soomjae.core.data.datastore.SESSION_DATA_STORE
+import com.parksupark.soomjae.core.data.datastore.SETTING_DATA_STORE
 import com.parksupark.soomjae.core.data.datastore.createSessionDataStore
+import com.parksupark.soomjae.core.data.datastore.createSettingDataStore
 import org.koin.core.module.Module
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 actual val platformCoreDataModule: Module = module {
     single(named(SESSION_DATA_STORE)) { createSessionDataStore() }
+    single(named(SETTING_DATA_STORE)) { createSettingDataStore() }
 }
