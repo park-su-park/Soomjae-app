cdc02c17afbd277445bd7808dfd74afe9ef9e13b
ruh0n <wns8256@naver.com>
2025-07-25 17:27:02 +0900

    refactor(auth): extract datasource from auth repository

--- features/auth/data/src/commonMain/kotlin/com/parksupark/soomjae/features/auth/data/AuthRepositoryImpl.kt
+++ features/auth/data/src/commonMain/kotlin/com/parksupark/soomjae/features/auth/data/AuthRepositoryImpl.kt
@@ -1,57 +1,39 @@
 package com.parksupark.soomjae.features.auth.data
 
 import arrow.core.Either
+import com.parksupark.soomjae.core.common.utils.mapToEmpty
 import com.parksupark.soomjae.core.domain.auth.models.AuthInfo
 import com.parksupark.soomjae.core.domain.auth.repositories.SessionRepository
 import com.parksupark.soomjae.core.domain.failures.DataFailure
-import com.parksupark.soomjae.core.remote.networking.post
-import com.parksupark.soomjae.features.auth.data.dto.CheckEmailAvailableRequest
-import com.parksupark.soomjae.features.auth.data.dto.CheckEmailAvailableResponse
-import com.parksupark.soomjae.features.auth.data.dto.EmailLoginRequest
-import com.parksupark.soomjae.features.auth.data.dto.EmailLoginResponse
-import com.parksupark.soomjae.features.auth.data.dto.RegisterRequest
+import com.parksupark.soomjae.features.auth.data.datasources.remote.RemoteAuthDataSource
 import com.parksupark.soomjae.features.auth.domain.AuthRepository
-import io.ktor.client.HttpClient
 
 class AuthRepositoryImpl(
-    private val httpClient: HttpClient,
+    private val remoteAuthDataSource: RemoteAuthDataSource,
     private val sessionRepository: SessionRepository,
 ) : AuthRepository {
     override suspend fun register(
         email: String,
         password: String,
         nickname: String,
-    ): Either<DataFailure.Network, Unit> = httpClient.post<RegisterRequest, Unit>(
-        route = "/v1/members/create-member",
-        body = RegisterRequest(
-            email = email,
-            password = password,
-            nickname = nickname,
-        ),
+    ): Either<DataFailure.Network, Unit> = remoteAuthDataSource.register(
+        email = email,
+        password = password,
+        nickname = nickname,
     )
 
     override suspend fun login(
         email: String,
         password: String,
-    ): Either<DataFailure.Network, Unit> = httpClient.post<EmailLoginRequest, EmailLoginResponse>(
-        route = "/auth/login",
-        body = EmailLoginRequest(
-            email = email,
-            password = password,
-        ),
-    ).map {
-        sessionRepository.set(
-            AuthInfo(accessToken = it.accessToken),
-        )
-    }
+    ): Either<DataFailure.Network, Unit> = remoteAuthDataSource.login(
+        email = email,
+        password = password,
+    ).also { result ->
+        result.onRight { response ->
+            sessionRepository.set(AuthInfo(accessToken = response.accessToken))
+        }
+    }.mapToEmpty()
 
     override suspend fun checkEmailAvailable(email: String): Either<DataFailure.Network, Boolean> =
-        httpClient.post<CheckEmailAvailableRequest, CheckEmailAvailableResponse>(
-            route = "/v1/members/check-duplicate-email",
-            body = CheckEmailAvailableRequest(
-                email = email,
-            ),
-        ).map {
-            !it.duplicate
-        }
+        remoteAuthDataSource.checkEmailAvailable(email)
 }
--- features/auth/data/src/commonMain/kotlin/com/parksupark/soomjae/features/auth/data/di/FeaturesDataModule.kt
+++ features/auth/data/src/commonMain/kotlin/com/parksupark/soomjae/features/auth/data/di/FeaturesDataModule.kt
@@ -1,6 +1,7 @@
 package com.parksupark.soomjae.features.auth.data.di
 
 import com.parksupark.soomjae.features.auth.data.AuthRepositoryImpl
+import com.parksupark.soomjae.features.auth.data.datasources.remote.RemoteAuthDataSource
 import com.parksupark.soomjae.features.auth.domain.AuthRepository
 import com.parksupark.soomjae.features.auth.domain.UserDataValidator
 import org.koin.core.module.dsl.singleOf
@@ -10,5 +11,6 @@ import org.koin.dsl.module
 val featuresAuthDataModule = module {
     singleOf(::UserDataValidator)
 
+    singleOf(::RemoteAuthDataSource)
     singleOf(::AuthRepositoryImpl).bind<AuthRepository>()
 }
