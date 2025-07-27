1d9ba36a3dbad808b3a1e4ed874f9c18f1320a9c
ruh0n <wns8256@naver.com>
2025-07-27 21:20:44 +0900

    feat(posts): add meeting post data class

---
+++ features/posts/meeting/domain/src/commonMain/kotlin/com/parksupark/soomjae/features/posts/common/domain/models/MeetingPost.kt
@@ -0,0 +1,15 @@
+package com.parksupark.soomjae.features.posts.common.domain.models
+
+import com.parksupark.soomjae.core.domain.models.Member
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
+
+@OptIn(ExperimentalTime::class)
+data class MeetingPost(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val createdAt: Instant,
+    val author: Member,
+    val category: Category,
+)
