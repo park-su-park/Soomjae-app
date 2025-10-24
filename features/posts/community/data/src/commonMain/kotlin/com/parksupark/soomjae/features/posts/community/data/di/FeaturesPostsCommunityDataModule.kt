c5eda09e94e67159e820a77a5411da079f81c451
ruh0n <wns8256@naver.com>
2025-10-24 16:11:13 +0900

    refactor(community): replace string literals with qualifiers for repositories

--- features/posts/community/data/src/commonMain/kotlin/com/parksupark/soomjae/features/posts/community/data/di/FeaturesPostsCommunityDataModule.kt
+++ features/posts/community/data/src/commonMain/kotlin/com/parksupark/soomjae/features/posts/community/data/di/FeaturesPostsCommunityDataModule.kt
@@ -8,24 +8,22 @@ import com.parksupark.soomjae.features.posts.community.data.repository.Community
 import com.parksupark.soomjae.features.posts.community.data.repository.CommunityLikeRepository
 import com.parksupark.soomjae.features.posts.community.data.repository.DefaultCommunityPostRepository
 import com.parksupark.soomjae.features.posts.community.data.repository.OfflineFirstCommunityPostLikeRepository
-import com.parksupark.soomjae.features.posts.community.domain.repository.COMMUNITY_COMMENT_REPOSITORY
-import com.parksupark.soomjae.features.posts.community.domain.repository.COMMUNITY_LIKE_REPOSITORY
+import com.parksupark.soomjae.features.posts.community.domain.di.PostsCommunityQualifier
 import com.parksupark.soomjae.features.posts.community.domain.repository.CommunityPostLikeRepository
 import com.parksupark.soomjae.features.posts.community.domain.repository.CommunityPostRepository
 import org.koin.core.module.dsl.singleOf
-import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 private val repositoriesModule = module {
     singleOf(::OfflineFirstCommunityPostLikeRepository).bind<CommunityPostLikeRepository>()
     singleOf(::DefaultCommunityPostRepository).bind<CommunityPostRepository>()
-    single(named(COMMUNITY_COMMENT_REPOSITORY)) {
+    single(PostsCommunityQualifier.COMMENT_REPOSITORY) {
         CommunityCommentRepository(
             httpClient = get(),
         )
     }.bind<CommentRepository>()
-    single(named(COMMUNITY_LIKE_REPOSITORY)) {
+    single(PostsCommunityQualifier.LIKE_REPOSITORY) {
         CommunityLikeRepository(
             httpClient = get(),
         )
