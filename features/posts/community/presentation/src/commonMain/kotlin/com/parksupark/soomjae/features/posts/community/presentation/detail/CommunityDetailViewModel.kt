26f20ed99a512321eec565c194900fbeed3e0a94
ruh0n <wns8256@naver.com>
2025-10-30 22:27:00 +0900

    chore(community): add TODO for error handling in post deletion

--- features/posts/community/presentation/src/commonMain/kotlin/com/parksupark/soomjae/features/posts/community/presentation/detail/CommunityDetailViewModel.kt
+++ features/posts/community/presentation/src/commonMain/kotlin/com/parksupark/soomjae/features/posts/community/presentation/detail/CommunityDetailViewModel.kt
@@ -77,6 +77,7 @@ class CommunityDetailViewModel(
 
             postRepository.deletePost(postId).fold(
                 ifLeft = {
+                    // TODO: error handling
                 },
                 ifRight = {
                     _eventChannel.send(CommunityDetailEvent.PostDeleted)
