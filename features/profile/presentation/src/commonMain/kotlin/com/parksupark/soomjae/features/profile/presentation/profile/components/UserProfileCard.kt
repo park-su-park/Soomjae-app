0de0a96c1d67b86a14dbbcc2a89ac7ad13110e4d
ruh0n <wns8256@naver.com>
2025-11-17 08:38:25 +0900

    feat(profile): enhance UserProfileCard with edit functionality

--- features/profile/presentation/src/commonMain/kotlin/com/parksupark/soomjae/features/profile/presentation/profile/components/UserProfileCard.kt
+++ features/profile/presentation/src/commonMain/kotlin/com/parksupark/soomjae/features/profile/presentation/profile/components/UserProfileCard.kt
@@ -1,112 +1,104 @@
 package com.parksupark.soomjae.features.profile.presentation.profile.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Message
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
 import com.parksupark.soomjae.core.presentation.designsystem.components.SoomjaeButton
+import com.parksupark.soomjae.core.presentation.designsystem.components.SoomjaeIcon
 import com.parksupark.soomjae.core.presentation.designsystem.components.SoomjaeSecondaryButton
+import com.parksupark.soomjae.core.presentation.designsystem.theme.AppTheme
 import com.parksupark.soomjae.core.presentation.designsystem.theme.SoomjaeTheme
-import com.parksupark.soomjae.core.presentation.ui.utils.imageRequest
+import com.parksupark.soomjae.core.presentation.ui.components.ProfileImage
 import com.parksupark.soomjae.features.profile.presentation.profile.model.UserUi
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 internal fun UserProfileCard(
     user: UserUi,
     modifier: Modifier = Modifier,
+    isMyProfile: Boolean = false,
+    onEditProfileClick: () -> Unit = { },
 ) {
     Column(
-        modifier = modifier.padding(16.dp),
+        modifier = modifier.background(SoomjaeTheme.colorScheme.background1)
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(24.dp),
     ) {
-        UserInfo()
+        UserInfo(user)
 
-        AdditionalInfos()
-
-        ExtraButtons()
+        if (isMyProfile) {
+            EditProfileButton(onClick = onEditProfileClick)
+        } else {
+            ExtraButtons()
+        }
     }
 }
 
 @Composable
-private fun UserInfo() {
+private fun UserInfo(user: UserUi) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        AsyncImage(
-            model = imageRequest { data("https://picsum.photos/200") },
-            contentDescription = null,
-            modifier = Modifier.size(64.dp).clip(RoundedCornerShape(20.dp)),
-        )
+        ProfileImage(imageUrl = user.profileImageUrl)
+        Spacer(modifier = Modifier.height(12.dp))
 
         Text(
-            text = "@Username",
-            style = SoomjaeTheme.typography.captionL.copy(color = SoomjaeTheme.colorScheme.text2),
+            text = user.nickname,
+            color = SoomjaeTheme.colorScheme.text2,
+            style = SoomjaeTheme.typography.title3,
         )
 
         Text(
             text = "누구나 쉽게 배우는 스프링 부트",
-            style = SoomjaeTheme.typography.captionM.copy(color = SoomjaeTheme.colorScheme.text1),
+            color = SoomjaeTheme.colorScheme.text1,
+            style = SoomjaeTheme.typography.captionS,
             textAlign = TextAlign.Center,
         )
     }
 }
 
 @Composable
-private fun AdditionalInfos() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        AdditionalInfoItem("89", "팔로잉", modifier = Modifier.weight(1f))
-
-        AdditionalInfoItem("152k", "팔로워", modifier = Modifier.weight(1f))
-
-        AdditionalInfoItem("25k", "좋아요", modifier = Modifier.weight(1f))
-    }
-}
-
-@Composable
-private fun AdditionalInfoItem(
-    numberString: String,
-    label: String,
+private fun EditProfileButton(
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(
-            text = numberString,
-            style = SoomjaeTheme.typography.title2.copy(color = SoomjaeTheme.colorScheme.text1),
-            textAlign = TextAlign.Center,
-        )
-
-        Text(
-            text = label,
-            style = SoomjaeTheme.typography.captionL.copy(color = SoomjaeTheme.colorScheme.text3),
-            textAlign = TextAlign.Center,
-        )
-    }
+    SoomjaeSecondaryButton(
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth(),
+        content = {
+            SoomjaeIcon(
+                imageVector = Icons.Outlined.Edit,
+                contentDescription = null,
+                tint = SoomjaeTheme.colorScheme.primary,
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text("프로필 수정")
+        },
+    )
 }
 
 @Composable
 private fun ExtraButtons(
-    onFollowClick: () -> Unit = { /* TODO: Handle follow action */ },
-    onMessageClick: () -> Unit = { /* TODO: Handle message action */ },
+    onFollowClick: () -> Unit = { },
+    onMessageClick: () -> Unit = { },
 ) {
     Row(
         horizontalArrangement = Arrangement.spacedBy(12.dp),
@@ -114,12 +106,44 @@ private fun ExtraButtons(
     ) {
         SoomjaeButton(onClick = onFollowClick) {
             Icon(Icons.Default.Add, null)
+            Spacer(modifier = Modifier.width(4.dp))
             Text("팔로우")
         }
 
         SoomjaeSecondaryButton(
             onClick = onMessageClick,
-            content = { Text("메시지") },
+            content = {
+                Icon(Icons.AutoMirrored.Outlined.Message, null)
+                Spacer(modifier = Modifier.width(4.dp))
+                Text("메시지")
+            },
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun UserProfileCard_MyProfilePreview() {
+    AppTheme {
+        UserProfileCard(
+            user = UserUi(
+                id = 1,
+                nickname = "Username",
+            ),
+            isMyProfile = true,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun UserProfileCard_OtherProfilePreview() {
+    AppTheme {
+        UserProfileCard(
+            user = UserUi(
+                id = 1,
+                nickname = "Username",
+            ),
         )
     }
 }
