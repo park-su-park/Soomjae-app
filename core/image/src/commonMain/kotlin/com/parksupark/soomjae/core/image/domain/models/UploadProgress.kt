2132ebae7a4900a89115ee306ead9925a389fd2b
ruh0n <wns8256@naver.com>
2025-11-17 22:07:00 +0900

    feat(image): add UploadAwareImage component for handling image upload states

--- core/image/src/commonMain/kotlin/com/parksupark/soomjae/core/image/domain/models/UploadProgress.kt
+++ core/image/src/commonMain/kotlin/com/parksupark/soomjae/core/image/domain/models/UploadProgress.kt
@@ -1,10 +1,12 @@
 package com.parksupark.soomjae.core.image.domain.models
 
+import androidx.compose.runtime.Immutable
 import com.parksupark.soomjae.core.domain.failures.DataFailure
 
 sealed interface UploadProgress {
     data object Idle : UploadProgress
 
+    @Immutable
     data class InProgress(val fraction: Float) : UploadProgress
 
     data class Success(val result: RemoteFile) : UploadProgress
