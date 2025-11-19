c59cefa756ea63d19448533d60da6e235f358f85
ruh0n <wns8256@naver.com>
2025-11-19 11:32:21 +0900

    fix(notification): change api path for deleting device token

--- core/notification/data/src/commonMain/kotlin/com/parksupark/soomjae/core/notification/data/service/KtorDeviceTokenService.kt
+++ core/notification/data/src/commonMain/kotlin/com/parksupark/soomjae/core/notification/data/service/KtorDeviceTokenService.kt
@@ -2,9 +2,9 @@ package com.parksupark.soomjae.core.notification.data.service
 
 import arrow.core.Either
 import com.parksupark.soomjae.core.domain.failures.DataFailure
+import com.parksupark.soomjae.core.notification.data.dto.request.DeleteDeviceTokenRequest
 import com.parksupark.soomjae.core.notification.data.dto.request.RegisterDeviceTokenRequest
 import com.parksupark.soomjae.core.notification.domain.service.DeviceTokenService
-import com.parksupark.soomjae.core.remote.networking.delete
 import com.parksupark.soomjae.core.remote.networking.post
 import io.ktor.client.HttpClient
 
@@ -26,8 +26,10 @@ class KtorDeviceTokenService(
     )
 
     override suspend fun unregisterToken(deviceId: String): Either<DataFailure.Network, Unit> =
-        httpClient.delete(
-            route = "/v1/fcm-token",
-            queryParameters = mapOf("device" to deviceId),
+        httpClient.post<DeleteDeviceTokenRequest, Unit>(
+            route = "/v1/fcm-token/delete",
+            body = DeleteDeviceTokenRequest(
+                deviceId = deviceId,
+            )
         )
 }
