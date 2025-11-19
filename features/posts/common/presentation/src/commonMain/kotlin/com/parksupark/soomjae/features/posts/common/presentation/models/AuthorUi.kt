aa9778578e7e75bead82ec6c13ff113bfebb4312
ruh0n <wns8256@naver.com>
2025-11-19 12:10:45 +0900

    fix(author): change profileImageUrl to non-nullable type

--- features/posts/common/presentation/src/commonMain/kotlin/com/parksupark/soomjae/features/posts/common/presentation/models/AuthorUi.kt
+++ features/posts/common/presentation/src/commonMain/kotlin/com/parksupark/soomjae/features/posts/common/presentation/models/AuthorUi.kt
@@ -5,12 +5,11 @@ import com.parksupark.soomjae.core.domain.models.Member
 data class AuthorUi(
     val id: String,
     val nickname: String,
-    val profileImageUrl: String?,
+    val profileImageUrl: String,
 )
 
 fun Member.toUi(): AuthorUi = AuthorUi(
     id = id,
     nickname = nickname,
-    profileImageUrl = profileImageUrl
-        ?: "https://picsum.photos/seed/${id.toLongOrNull() ?: 0}/200/200",
+    profileImageUrl = profileImageUrl,
 )
